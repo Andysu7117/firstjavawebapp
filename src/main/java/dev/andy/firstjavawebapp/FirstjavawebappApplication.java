585134a21a644992d766b5a19938f4e6ade087eb
Andy <andy.su7117@gmail.com>
2025-06-30 22:15:08 +1000

    spring boot initialiser

---
+++ src/main/java/dev/andy/firstjavawebapp/FirstjavawebappApplication.java
@@ -0,0 +1,13 @@
+package dev.andy.firstjavawebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstjavawebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstjavawebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/andy/firstjavawebapp/FirstjavawebappApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.andy.firstjavawebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstjavawebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
