a7694b60368850a2c208d8959f61f5264e5ca7f0
Andy <andy.su7117@gmail.com>
2025-07-01 22:13:30 +1000

    changed to use records

--- src/main/java/dev/andy/firstjavawebapp/workout/Set.java
+++ src/main/java/dev/andy/firstjavawebapp/workout/Set.java
@@ -1,83 +1,11 @@
 package dev.andy.firstjavawebapp.workout;
 
 import java.time.LocalDateTime;
-import java.util.Objects;
 
-public class Set {
-    private String exercise;
-    private Integer reps;
-    private LocalDateTime setStarted;
-    private LocalDateTime setCompleted;
-    private Integer duration;
-
-    public Set(String exercise, Integer reps, LocalDateTime setStarted, LocalDateTime setCompleted, Integer duration) {
-        this.exercise = exercise;
-        this.reps = reps;
-        this.setStarted = setStarted;
-        this.setCompleted = setCompleted;
-        this.duration = duration;
-    }
-
-    public String getExercise() {
-        return this.exercise;
-    }
-
-    public Integer getReps() {
-        return this.reps;
-    }
-
-    public LocalDateTime getStarted() {
-        return this.setStarted;
-    }
-
-    public LocalDateTime getCompleted() {
-        return this.setCompleted;
-    }
-
-    public Integer getDuration() {
-        return this.duration;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        Set set = (Set) o;
-        if (!Objects.equals(exercise, set.getExercise())) {
-            return false;
-        }
-
-        if (!Objects.equals(reps, set.getReps())) {
-            return false;
-        }
-
-        if (!Objects.equals(setStarted, set.getStarted())) {
-            return false;
-        }
-
-        if (!Objects.equals(setCompleted, set.getCompleted())) {
-            return false;
-        }
-
-        if (!Objects.equals(duration, set.getDuration())) {
-            return false;
-        }
-
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "Set{" +
-                "Exercise=" + exercise +
-                ", Reps=" + reps +
-                ", Started=" + setStarted +
-                ", Completed" + setCompleted +
-                ", Duration" + duration +
-                "}";
-    }
-}
+public record Set(
+    String exercise,
+    Integer reps,
+    LocalDateTime setStarted,
+    LocalDateTime setCompleted,
+    Integer duration
+) {}
--- src/main/java/dev/andy/firstjavawebapp/workout/Workout.java
+++ src/main/java/dev/andy/firstjavawebapp/workout/Workout.java
@@ -1,43 +1,8 @@
 package dev.andy.firstjavawebapp.workout;
 
 import java.util.List;
-import java.util.Objects;
-import java.util.ArrayList;
 
-public class Workout {
-    private List<Set> sets = new ArrayList<>();
-    private String workoutName;
-
-    public Workout(List<Set> sets, String workOutName) {
-        this.sets = sets;
-        this.workoutName = workOutName;
-    }
-
-    public List<Set> getSets() {
-        return this.sets;
-    }
-
-    public String getWorkoutName() {
-        return this.workoutName;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-
-        if (o == null || this.getClass() != o.getClass()) {
-            return false;
-        }
-
-        Workout workout = (Workout) o;
-        return Objects.equals(sets, workout.getSets()) && Objects.equals(workoutName, workout.getWorkoutName());
-    }
-
-    @Override
-    public String toString() {
-        return "Workout{" +
-                "WorkoutName=" + workoutName +
-                ", sets=" + sets +
-                "}";
-    }
-}
+public record Workout(
+    List<Set> sets,
+    String workoutName
+) {}
